acd0e7878304f94c3d17339f8f259298515156df
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-23 11:12:34 +0000

    Create hour.c

---
+++ hour.c
@@ -0,0 +1,13 @@
+#include<stdio.h.
+int main()
+{
+int hr,hr1,hr2,min1,min2,min;
+printf("first time:");
+scanf("%d%d",&hr1,&min1);
+printf("second time");
+scanf("%d%d",&hr2,&min2);
+hr=hr1-hr2;
+min=min1-min2;
+printf("rem hour is %d and rem min is %d",hr,min);
+return 0;
+}
