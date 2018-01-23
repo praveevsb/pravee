c85b0823c465d755520a2f61cefd0226f6de47a5
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-23 10:14:14 +0000

    Create max10.c

---
+++ max10.c
@@ -0,0 +1,20 @@
+#include<stdio.h.
+int main()
+{
+int a[10],i,max;
+printf("enter 10 values:");
+for(i=0;i<10;i++)
+{
+scanf("%d",&a[i]);
+}
+max=a[0];
+for(i=0;i<10;i++0
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+}
+printf("greatest num is %d:",max);
+return 0;
+}
