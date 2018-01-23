37e7b66934a981b33e9ec3a8e85cd7355d6eac64
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-23 10:55:23 +0000

    Create minarray.c

---
+++ minarray.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+int n,a[100],i,min;
+printf("enter the number:");
+scanf("%d",&n);
+printf("enter the array values:");
+for(i=0;i<n;i++)
+{
+scanf("%d",&a[i]);
+}
+min=a[0];
+for(i=0;i<n;i++)
+{
+if(a[i]>max)
+min=a[i];
+}
+printf("greatest num is %d",count+1);
+return 0;
+}
