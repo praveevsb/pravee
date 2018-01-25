959d5808a965c6fc982d65003caece6bff6617c6
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-25 11:16:33 +0000

    Create hunter50.c

---
+++ hunter50.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int n1,n2,a,b,count=0;
+printf("enter number1:");
+scanf("%d",&n1);
+printf("enter number2:");
+scanf("%d",&n2);
+n1=a;
+n2=b;
+while(n1>=n2)
+{
+n1=n1-n2;
+count++:
+}
+printf("quotient is %d and remainder is %d",count,n1);
+return 0;
+}
