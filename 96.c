be4eb76d73da28d5eb6f447adb7c1c379ea433fc
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-01-26 16:29:02 +0530

    Create 96.c

---
+++ 96.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+int i,n,count=0;
+printf("ENTER THE NUM:");
+scanf("%d",&n);
+for(i=2;i<n;i++)
+{
+if((n%i)==0)
+++count;
+}
+if(count==0)
+printf("num is prime");
+else
+printf("num is composite");
+return 0;
+}
