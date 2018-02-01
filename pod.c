a25ad7f0a0c4d080d5b865e3ac4b239bfc2d0838
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-01 09:18:00 +0000

    Create pod.c

---
+++ pod.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int n, t;
+long p=1;
+t=n;
+printf("enter the number:");
+scanf("%d",&n);
+while(n!=0)
+{
+p*=n%10;
+n=n/10;
+}
+printf("product is %ld",p);
+return 0;
+}
