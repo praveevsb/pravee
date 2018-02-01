f7da6044d9dc9871dc136bc38280d31f75c10541
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-01 09:27:00 +0000

    Create 94.c

---
+++ 94.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+           int n[10],k,N,i;
+           scanf("%d%d",&N,&k);
+           for(i=1;i<N;i++)
+           scanf("%d",&n[i]);
+           printf("%d",n[k]);
+	return 0;
+}
