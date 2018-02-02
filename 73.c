0f0030b07c4d3e482b11949bedfcf237212c307d
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-02 14:25:03 +0530

    Create 73.c

---
+++ 73.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter two numbers");
+	scanf("%d%d",&b,&c);
+	printf("enter the number");
+	scanf("%d",&a);
+	if(a>b&&a<c)
+	{
+		printf("yes it is between");
+ 
+	}
+else
+{
+	printf("no it is not present");
+}}
