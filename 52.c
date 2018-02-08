2008446b6c8df5ed2685e01ff70f21f018d242c6
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-08 14:56:38 +0530

    Create 52.c

---
+++ 52.c
@@ -0,0 +1,42 @@
+#include<stdio.h.
+int main()
+{
+int num;printf("enter the num");
+scanf("%d",&num);
+switch(num)
+{
+case1:
+printf("one");
+break;
+case2:
+printf("two");
+break;
+case3:
+printf("three");
+break;
+case4:
+printf("four");
+break;
+case5:
+printf("five');
+break;
+case6:
+printf("six");
+break;
+case7:
+printf("seven");
+break;
+case8:
+printf("eight");
+break;
+case9:
+printf("nine");
+break;
+case10:
+printf("ten");
+break;
+default:
+printf("number");
+}
+return 0;
+}
