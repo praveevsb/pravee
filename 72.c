39c39564ab997b9ba3e652536f459ba79210604a
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-08 14:24:26 +0530

    Create 72.c

---
+++ 72.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+int i,p=0;
+char b[100];
+printf("enter the word");
+gets(b);
+for(i=1;a[i]!='\0';i++)
+{
+if((a[i]='a')||(a[i]='e')||(a[i]='i')||(a[i]='o')||(a[i]='u')||(a[i]='A')||(a[i]='E')||(a[i]='I')||(a[i]='O')||(a[i]='U'))
+{
+f=1;
+}
+}
+if(f>=1)
+{
+printf("vowel");
+}
+else
+{
+printf("no vowel");
+}
+return 0;
+}
