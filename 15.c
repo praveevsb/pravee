a15b83b9f664420fab36a89bbb3f1b09c98970b3
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-02-13 14:49:27 +0530

    Create 15.c

---
+++ 15.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+int N,K,i;
+printf("ENTER THE NUMBERS");
+scanf("%d%d",&N,&K);
+for(i=N+1;i<K;i++)
+{
+if(i%2==0)
+{
+    printf("%d\t",i);
+}
+
+}
+
+
+return 0;
+}
