44543090c7fe5aaabdf3932a4810394ede4ce844
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-03-15 15:59:17 +0530

    Create 62.c

---
+++ 62.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int str;
+printf("enter the string");
+scanf("%d",&str);
+if((str==0)||(str==1))
+{
+printf("binary representation");
+}
+else
+{
+printf("not a binary representation");
+}
+return 0;
+}
