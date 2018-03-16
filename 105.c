8fca1f4d84adc988f9a1c52a2f1d22cf20cfc859
praveevsb <31505742+praveevsb@users.noreply.github.com>
2018-03-16 16:05:08 +0530

    Create 105.c

---
+++ 105.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+   int n;
+   printf("enter the number");
+   scanf("%d",&n);
+   char bb;
+   bb=(64+n);
+   printf("corresponding alphabet is %c",bb);
+    return 0;
+}
